a80d9ad7fe0cdf753731a40c0f0c0e8a1d3cd493
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-16 16:37:07 +0300

    About page updated

--- personal_portfolio/src/Components/About/About.jsx
+++ personal_portfolio/src/Components/About/About.jsx
@@ -1,8 +1,27 @@
+// export default function About() {
+//   return (
+//     <section id="about">
+//       <h2>About Me</h2>
+//       <p>Public health professional transitioning to software development.</p>
+//     </section>
+//   );
+// }
+
 export default function About() {
   return (
-    <section id="about">
-      <h2>About Me</h2>
-      <p>Public health professional transitioning to software development.</p>
+    <section id="about" className="section bg-dark-1">
+      <div className="container">
+        <h2 className="text-white">About Me</h2>
+
+        <p className="text-white-50">
+          I'm Berhanu Mengesha, Public Health Professional and Web Developer...
+        </p>
+
+        <a href="/Berhanu_Mengesha_CV.pdf" download className="btn btn-primary">
+          Download CV
+        </a>
+      </div>
     </section>
   );
 }
+
