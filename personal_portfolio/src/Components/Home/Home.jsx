3a94232c0d3606628805ca0eb1ea20cb676c929f
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-16 16:35:35 +0300

    Homepage updated

--- personal_portfolio/src/Components/Home/Home.jsx
+++ personal_portfolio/src/Components/Home/Home.jsx
@@ -1,8 +1,32 @@
+// export default function Home() {
+//   return (
+//     <section id="home">
+//       <h1>Welcome to My Portfolio</h1>
+//       <p>I am Berhanu Mengesha – Web Developer</p>
+//     </section>
+//   );
+// }
+
 export default function Home() {
   return (
     <section id="home">
-      <h1>Welcome to My Portfolio</h1>
-      <p>I am Berhanu Mengesha – Web Developer</p>
+      <div className="hero-wrap">
+        <div
+          className="hero-bg"
+          style={{ backgroundImage: "url('/images/bc1.jpg')" }}
+        ></div>
+
+        <div className="hero-content section d-flex min-vh-100">
+          <div className="container my-auto text-center">
+            <h2 className="text-16 text-white">I'm Berhanu Mengesha</h2>
+            <p className="text-light">Based in Debre Birhan, Ethiopia</p>
+
+            <a href="#contact" className="btn btn-primary">
+              Hire Me
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
