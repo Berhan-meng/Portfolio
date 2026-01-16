8ee3f4e4399b6aacc0521ab326f20e9be2164baa
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-16 16:15:43 +0300

    About page added

--- personal_portfolio/src/App.jsx
+++ personal_portfolio/src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "../src/Components/Navbar/Navbar.jsx";
 import Home from "../src/Components/Home/Home.jsx";
+import About from "../src/Components/About/About.jsx";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Home />
-      {/* <About /> */}
+      <About />
       {/* <Services /> */}
       {/* <Resume /> */}
       {/* <Portfolio /> */}
