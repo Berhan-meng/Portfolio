29d5cbf02a9987c74955ec8180652f7704f2d9e3
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-16 16:13:35 +0300

    home component added

--- personal_portfolio/src/App.jsx
+++ personal_portfolio/src/App.jsx
@@ -1,16 +1,17 @@
 import Navbar from "../src/Components/Navbar/Navbar.jsx";
+import Home from "../src/Components/Home/Home.jsx";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      {/* <Home />
-      <About />
-      <Services />
-      <Resume />
-      <Portfolio />
-      <Contact />
-      <Footer /> */}
+      <Home />
+      {/* <About /> */}
+      {/* <Services /> */}
+      {/* <Resume /> */}
+      {/* <Portfolio /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
     </>
   );
 }
---
+++ personal_portfolio/src/Components/Home/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section id="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>I am Berhanu Mengesha – Web Developer</p>
+    </section>
+  );
+}
