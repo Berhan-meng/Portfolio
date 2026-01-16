a87b61ca740fcfab8cabc1b3906c1ff19db0421f
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-16 16:20:04 +0300

    Resume Section added

--- personal_portfolio/src/App.jsx
+++ personal_portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "../src/Components/Navbar/Navbar.jsx";
 import Home from "../src/Components/Home/Home.jsx";
 import About from "../src/Components/About/About.jsx";
 import Services from "../src/Components/Services/Services.jsx";
+import Resume from "../src/Components/Resume/Resume.jsx";
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
       <Home />
       <About />
       <Services />
-      {/* <Resume /> */}
+      <Resume />
       {/* <Portfolio /> */}
       {/* <Contact /> */}
       {/* <Footer /> */}
---
+++ personal_portfolio/src/Components/Resume/Resume.jsx
@@ -0,0 +1,8 @@
+export default function Resume() {
+  return (
+    <section id="resume">
+      <h2>Resume</h2>
+      <p>Experience in Public Health and Software Development.</p>
+    </section>
+  );
+}
