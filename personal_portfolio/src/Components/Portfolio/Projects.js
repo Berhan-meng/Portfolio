f09e56b65143665de456704c19f38325e12b435d
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-18 19:27:15 +0300

    Images for projects link  and skill.js file added

---
+++ personal_portfolio/src/Components/Portfolio/Projects.js
@@ -0,0 +1,44 @@
+import amazoneimage from "../../Assets/Amazon4.png";
+import evangadiforum from "../../Assets/evangadi.png";
+import youtubeimg from "../../Assets/youtubeclone.png";
+import drumkit from "../../Assets/drum_kit.png";
+import rockpaperscissor from "../../Assets/rock_paper_scissor.png";
+export const projects = [
+  {
+    title: "Netflix-Clone",
+    category: "React Js",
+    image:
+      "https://images.fastcompany.com/image/upload/f_webp,q_auto,c_fit/wp-cms-2/2024/04/p-1-91108938-netflix-reasserts-dominance-new-model.jpg",
+    link: "https://berhan-meng.github.io/Netflix-clone-2025/",
+  },
+  {
+    title: "Amazon-Ecommerce-clone",
+    category: "React Js",
+    image: amazoneimage,
+    link: "https://amazoneclone.birhann.com/",
+  },
+  {
+    title: "Evangadi Forum",
+    category: "React Js",
+    image: evangadiforum,
+    link: "https://evangadiforum.birhann.com/",
+  },
+  {
+    title: "Rock-paper-Scissor Game",
+    category: "JS/DOM",
+    image: rockpaperscissor,
+    link: "https://birhan-m-rock-paper-scisor-game.netlify.app/",
+  },
+  {
+    title: "Drum-kit",
+    category: "JS-DOM",
+    image: drumkit,
+    link: "https://birhan-m-drum-kit.netlify.app/",
+  },
+  {
+    title: "YouTube-Clone",
+    category: "HTML/CSS",
+    image: youtubeimg,
+    link: "https://birhanm-youtube-clone.netlify.app",
+  },
+];
