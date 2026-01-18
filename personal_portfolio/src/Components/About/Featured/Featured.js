ee974ec354c372d125a5bda263bcadf1b4bbc4ef
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-18 19:45:59 +0300

    Featured part added in the Resume page

---
+++ personal_portfolio/src/Components/About/Featured/Featured.js
@@ -0,0 +1,22 @@
+export const featureList = [
+  {
+    param: "Years Experiance",
+    init: 0,
+    num: 9,
+  },
+  {
+    param: "Happy Clients",
+    init: 0,
+    num: 500,
+  },
+  {
+    param: "Projects Done",
+    init: 0,
+    num: 10,
+  },
+  {
+    param: "Get Awards",
+    init: 0,
+    num: 5,
+  },
+];
---
+++ personal_portfolio/src/Components/About/Featured/Featured.jsx
@@ -0,0 +1,26 @@
+import { featureList } from "./Featured.js";
+
+export default function Featured() {
+  console.log(featureList);
+  return (
+    <div class="brands-grid separator-border mt-5">
+      <div class="row">
+        {featureList.map(({ param, num, init }) => {
+          return (
+            <div class="col-6 col-md-3">
+              <div class="featured-box text-center">
+                <h4 class="text-12 text-muted mb-0">
+                  <span class="counter" data-from={init} data-to={num}>
+                    {num}
+                  </span>
+                  +
+                </h4>
+                <p class="mb-0">{param}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
