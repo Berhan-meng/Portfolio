c39e15ab1b09ae82904658135bf7059cbf90f2d0
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-20 23:42:38 +0300

    backend url added to axios

--- personal_portfolio/src/Components/Api/axios.js
+++ personal_portfolio/src/Components/Api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_URL;
+// const apiUrl = import.meta.env.VITE_API_URL;
 
 const axiosBase = axios.create({
-  baseURL: apiUrl,
+  baseURL: "https://portfoliobackend.birhann.com/api",
 });
 
 export default axiosBase;
